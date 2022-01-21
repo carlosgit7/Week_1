43e95ea0f16df4457f9af34b632b615ed7ad178d
Carlos <cgonz490@stu.southtexascollege.edu>
2022-01-21 10:58:25 -0600

    init

---
+++ app.js
@@ -0,0 +1,67 @@
+// DRY Version, Don't Repeat Yourself
+
+const todos = [];
+
+const get = (elements) =>
+  elements.map((element) => document.getElementById(element));
+
+const [pendingList, completedList, addForm, newTodo] = get([
+  "pendingList",
+  "completedList",
+  "addForm",
+  "newTodo",
+]);
+
+const newList = [
+  {
+    element: pendingList,
+    status: "pending",
+  },
+  {
+    element: completedList,
+    status: "done",
+  },
+];
+
+const cssClasses = {
+  pending:
+    "bg-gray-300 w-2/3 text-center text-red-600 text-lg rounded py-1 border-2 border-red-600 transition transform ease-in-out duration-400 hover:bg-red-500 hover:text-white hover:scale-105 cursor-pointer",
+  done:
+    "bg-gray-300 w-2/3 text-center text-green-600 text-lg rounded py-1 border-2 border-green-700 transition transform ease-in-out duration-400 hover:bg-green-500 hover:text-white hover:scale-105 cursor-pointer",
+};
+
+const updateTodos = () => {
+  newList.forEach((list) => {
+    const filteredTodos = todos.filter((todo) => todo.status === list.status);
+
+    list.element.innerHTML = "";
+    filteredTodos.forEach((todo) => {
+      const item = document.createElement("li");
+      item.className = cssClasses[list.status];
+      item.innerText = todo.text;
+      item.id = todo.id;
+      list.element.appendChild(item);
+    });
+  });
+};
+
+addForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  todos.push({
+    id: Math.floor(Math.random() * 100000).toString(),
+    text: newTodo.value,
+    status: "pending",
+  });
+  newTodo.value = "";
+  updateTodos();
+});
+
+pendingList.addEventListener("click", (event) => {
+  todos.find((todo) => todo.id === event.target.id).status = "done";
+  updateTodos();
+});
+
+completedList.addEventListener("click", (event) => {
+  todos.find((todo) => todo.id === event.target.id).status = "pending";
+  updateTodos();
+});
